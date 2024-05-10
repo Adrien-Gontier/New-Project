d2a6fe563629941aad19427b693c8285b26e0e5b
Adrien Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2024-05-10 09:23:41 +0200

    Create Simple_CalculatorApp.swift
    Initial Commit

---
+++ Simple Calculator/Simple_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_CalculatorApp.swift
+//  Simple Calculator
+//
+//  Created by Adrien on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Simple_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
