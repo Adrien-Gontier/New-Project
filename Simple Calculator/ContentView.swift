add95d3ac06b593a7212b8e444f80964b7292e73
Adrien Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2024-06-04 20:58:57 +0200

    Update ContentView.swift
    ContentView.swift

--- Simple Calculator/ContentView.swift
+++ Simple Calculator/ContentView.swift
@@ -11,7 +11,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var tempStrint = ""
+    @State var tempString = ""
     @State var result : Int = 0
         var body: some View {
             Text("Simple Calculator")
@@ -45,7 +45,7 @@ struct ContentView: View {
             
             Button {
                     action: do {
-                        tempStrint = tempStrint + "2"
+                        tempString = tempString + "2"
                     }
                     }label: {
                         Text("2")
@@ -60,6 +60,22 @@ struct ContentView: View {
                             .frame(width: 50, height: 60)
                     }.padding(.bottom, 30)
             
+            Button {
+                    action: do {
+                        tempString = tempString + "3"
+                    }
+                    }label: {
+                        Text("3")
+                            .padding([.top, .bottom], 15)
+                            .padding([.trailing, .leading], 25)
+                            .font(.system(size: 25))
+                            .foregroundColor(.blue)
+                            .background(.white)
+                            .overlay(RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.blue, lineWidth: 5))
+                            .fixedSize()
+                            .frame(width: 50, height: 60)
+                    }.padding(.bottom, 30)
             
     }
 }
